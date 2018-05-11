1250caf7ff7538a2641aec34659e71f64255f206
w1ndmill <5065829+w1ndmill@users.noreply.github.com>
2018-05-11 12:20:51 -0600

    Added use of config file, -f overrides config

--- swampchurn.py
+++ swampchurn.py
@@ -3,13 +3,21 @@
 #
 # TODO:
 # - Chat monitoring
-# - Connect monitoring
 # - Keyword detection
-# - rad
+# - Prompt to continue if forcelogs doesn't exist
 
 from logClasses import swampLogs, logEntry
-import os.path
+from os.path import isfile as isFile
+from sys import exit
 import argparse
+import configparser
+
+def verifyFile(filename, fromParser = True):
+    if not isFile(filename):
+        if fromParser: raise argparse.ArgumentTypeError(f"File {filename} not found. Aborting...")
+        print(f"File {filename} not found. Aborting...")
+        exit()
+    return filename
 
 def main():
     desc = """
@@ -18,7 +26,7 @@ def main():
             (_-< V  V / _` | '  \| '_ \/ _| ' \| | | '_| ' \\
             /__/\_/\_/\__,_|_|_|_| .__/\__|_||_\_,_|_| |_|_|
                                  |_|
-                      version 0.2 -- slipperyslope
+                    version 0.2 -- by slipperyslope
     """
     parser = argparse.ArgumentParser(description=desc,formatter_class=argparse.RawTextHelpFormatter)
     filegroup = parser.add_mutually_exclusive_group()
@@ -27,20 +35,25 @@ def main():
     parser.add_argument('-s', '--addSpray', action='store', help = 'add/overwrite spray id to database')
     parser.add_argument('-v', '--addVideo', action='store', help = 'add/overwrite video id to database')
 
-    filegroup.add_argument('-n', action='store_false', help='prevent building of logs')
-    filegroup.add_argument('-f', help = 'force raw logs file')
-    parser.parse_args()
+    filegroup.add_argument('-n', '--noBuild', action='store_false', help='prevent building of logs')
+    filegroup.add_argument('-f', '--forceLogs', type=verifyFile,  help = 'override raw log file from config')
+    args = parser.parse_args()
 
-    rawLogs = ""
+    config = configparser.ConfigParser()
+    if isFile("settings.ini"):
+        config.read("settings.ini")
+    else:
+        with open("settings.ini",'w+') as newSettings:
+            newSettings.write("[Paths]\nrawLogs = \ndatabase = ")
+        print("No settings file found. New \"settings.ini\" created")
+        exit()
 
-    if not rawLogs:
-        rawLogs = input("Enter log file name: ")
-    while not os.path.isfile(rawLogs):
-        rawLogs = input("Error: File \"" + rawLogs + "\" not found\nEnter log file name: ")
-    print("Log file \"" + rawLogs + "\" found. Processing...")
+    if args.forceLogs: rawLogs = args.forceLogs
+    else: rawLogs = verifyFile(config["Paths"]["rawLogs"], False)
 
     logs = swampLogs(rawLogs)
     logs.build()
+
     videos, sprays = logs.getLists("video","spray")
     for video in videos:
         print(video)
